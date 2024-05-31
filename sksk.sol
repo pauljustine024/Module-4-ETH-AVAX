0082ac23aea62216066df1051e243c56e2e74d10
pauljustine024 <128199663+pauljustine024@users.noreply.github.com>
2024-05-31 13:48:26 +0800

    sksk.sol

--- sksk.sol
+++ sksk.sol
@@ -4,41 +4,40 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract ExtremeToken is ERC20, Ownable {
 
     mapping(uint256 => uint256) public ShopPrices;
 
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
-        ShopPrices[1] = 100;
-        ShopPrices[2] = 60;
-        ShopPrices[3] = 30;
-        ShopPrices[4] = 10;
+    constructor() ERC20("Extreme", "EXT") Ownable(msg.sender) {
+        ShopPrices[1] = 150;
+        ShopPrices[2] = 80;
+        ShopPrices[3] = 40;
+        ShopPrices[4] = 20;
     }
 
-
-    function mintDGN(address _to, uint256 _amount) public onlyOwner {
+    function mintEXT(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
     }
 
-    function transferDGN(address _to, uint256 _amount) public {
+    function transferEXT(address _to, uint256 _amount) public {
         require(balanceOf(msg.sender) >= _amount, "Transfer Failed: Insufficient balance.");
         approve(msg.sender, _amount);
         transferFrom(msg.sender, _to, _amount);
     }
 
     function showShopItems() external pure returns (string memory) {
-        string memory saleOptions = "The items on sale: {1} Degen NFT (100) {2} Degen T-shirt & Hoodie (60) {3} Random IN-GAME Item (30) {4} Degen Sticker (10)";
+        string memory saleOptions = "The items on sale: {1} Extreme NFT (150) {2} Extreme Gear (80) {3} Random Extreme IN-GAME Item (40) {4} Extreme Sticker (20)";
         return saleOptions;
     }
 
-    function redeemDGN(uint256 _item) public {
+    function redeemEXT(uint256 _item) public {
         require(ShopPrices[_item] > 0, "Item is not available.");
         require(_item <= 4, "Item is not available.");
         require(balanceOf(msg.sender) >= ShopPrices[_item], "Redeem Failed: Insufficient balance.");
         transfer(owner(), ShopPrices[_item]);
     }
     
-    function burnDGN(uint256 _amount) public {
+    function burnEXT(uint256 _amount) public {
         require(balanceOf(msg.sender) >= _amount, "Burn Failed: Insufficient balance.");
         approve(msg.sender, _amount);
         _burn(msg.sender, _amount);
@@ -52,5 +51,4 @@ contract DegenToken is ERC20, Ownable {
         return 0;
     }
 
-    
 }
